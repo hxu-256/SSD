61f7134d15534ee7074ccdccbe8b802317b822af
Yurong <43307048+YurongChen1998@users.noreply.github.com>
2025-08-27 19:39:05 +0800

    1

--- main_SimuData.py
+++ main_SimuData.py
@@ -69,15 +69,15 @@ if __name__ == '__main__':
     parser.add_argument('--iter_num',     default = 30,             help="Number of ADMM iterations")
     parser.add_argument('--Epoc_num',     default = 1200,           help="Number of MSD iterations")
     parser.add_argument('--scale',        default = 1.0,            help="Factor of scaling mesurements")
-    parser.add_argument('--rho',          default = 20.,            help="Factor of dual variable")
-    parser.add_argument('--lambda_S',     default = 1,              help="Factor of sparse noise")
+    parser.add_argument('--rho',          default = 10,             help="Factor of dual variable")
+    parser.add_argument('--lambda_S',     default = 10,             help="Factor of sparse noise")
     parser.add_argument('--lambda_R',     default = 4.0,            help="Factor of TV/STV regularization")
     parser.add_argument('--lambda_STV',   default = 0.4,            help="Ratio of STV regularization")
     parser.add_argument('--lambda_D',     default = 0.8,            help="Decay factor of lambda_R")
     args = parser.parse_args()
 
     if args.Dataset   == 'Simu':
-        data_list     =  ['Chessboard_data'] #'Pattern_data', 'Gaussian_data', 'Chessboard_data', 'PS_ball_data', 'CM_layer10'
+        data_list     =  ['Gaussian_data'] #'Pattern_data', 'Gaussian_data', 'Chessboard_data', 'PS_ball_data'
     else:
         print("---------- Ensure the name of dataset ----------") 
 
@@ -90,8 +90,6 @@ if __name__ == '__main__':
             args.iter_num, args.lambda_R, args.lambda_STV, args.lambda_D = 30, 8.0, 0.2, 0.8
         elif file_name == 'Chessboard_data':
             args.iter_num, args.lambda_R, args.lambda_STV, args.lambda_D = 30, 5.0, 0.4, 0.8
-        elif file_name == 'CM_layer10':
-            args.lambda_S, args.lambda_R, args.lambda_STV, args.lambda_D = 1000, 0.05, 0.95, 0.95
         else:
             args.lambda_R, args.Epoc_num = parser.get_default('lambda_R'), parser.get_default('Epoc_num')
         
